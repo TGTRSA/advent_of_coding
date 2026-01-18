b2a28cea0c05a57fb8ad76d82f0d8d0a89370eeb
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-01-18 17:42:12 +0000

    Issue with the conversion to int still but wrote the function to check for the valid id combinations

--- c++/2025/day2.cpp
+++ c++/2025/day2.cpp
@@ -1,13 +1,9 @@
 
-#include <cstring>
+#include <cstdint>
+
 #include <iostream>
 #include <string>
 #include <vector>
-
-enum class int_type {
-    EVEN,
-    ODD,
-};
 
 struct Separators{
     char id_separator=',';
@@ -24,8 +20,50 @@ Separators separator;
 std::vector<std::string> valid_ids;
 std::string string_chunk = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224, 1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
-bool is_valid(std::string id_chunk, int id_len, enum int_type){
-    
+bool compare_sets(std::string set1, std::string set2){
+    if(set1==set2){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+bool check_first_two(std::string id_chunk){
+
+    if(id_chunk[0]==id_chunk[1]){return true;} 
+    else{return false;}
+        
+}
+
+bool is_valid(std::string id_chunk, int id_len){
+    std::string set1;
+    std::string set2;
+    int32_t set = 0;
+    bool duplicate;
+    if(id_len==2){
+        duplicate = check_first_two(id_chunk);
+        return duplicate;
+    }else{
+        if (id_len%2==0) 
+        {           
+            int half = id_len/2;
+            for(int i=0;i<half;i++){
+                if(set==0){
+                    set1+=id_chunk[i];
+                    if(i==half){
+                        i=half;
+                        half=half*2;
+                        set=1;
+                    }
+                }else{
+                    set2+=id_chunk[i];
+                }
+            }
+            duplicate = compare_sets(set1, set2);
+            
+        }
+        return duplicate;
+    }
 }
 
 void print_nl(){
@@ -40,18 +78,20 @@ bool is_even(int value){
     }
 }
 
-void is_valid(Boundaries boundaries) {
+void valid_id(Boundaries boundaries) {
     int loop_int = 0; 
     for(int i=boundaries.lower;i<boundaries.upper;i++){
         std::string string_int = std::to_string(i);
         int id_length = string_int.length();
         if(is_even(id_length)){
-            int loop_upper = id_length/2;
-            for(int u=0;u<loop_upper;u++){
-                bool validity = is_valid(string_int,id_length, int_type::EVEN);
+            bool duplicate = is_valid(string_int,id_length);
+            if(duplicate==true){
+                std::cout << string_int << " is valid";
+                print_nl();
             }
         }
     }
+   
 }
 
 Boundaries get_boundary_conditions(std::string id_range){
@@ -81,6 +121,7 @@ void loop_through_ranges(std::vector<std::string> ID_array) {
             print_nl();
             Boundaries boundary_conidtions = get_boundary_conditions(range);
             std::cout << "Lower bound: " << boundary_conidtions.lower << std::endl << "Upper bound: " << boundary_conidtions.upper << std::endl;    
+            valid_id(boundary_conidtions);
         }
         range = " ";
     }
