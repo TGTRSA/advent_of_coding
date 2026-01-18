5002cdfcc96356e4d982bf6a345af7ae1e8b1e7d
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-01-18 16:28:31 +0000

    Going to use even/odd checks to split the ids into sections and check the individual sections

--- c++/2025/day2.cpp
+++ c++/2025/day2.cpp
@@ -4,6 +4,11 @@
 #include <string>
 #include <vector>
 
+enum class int_type {
+    EVEN,
+    ODD,
+};
+
 struct Separators{
     char id_separator=',';
     char range_separator='-';
@@ -14,14 +19,40 @@ struct Boundaries {
     int lower;
 };
 
-Separators separator;
 
+Separators separator;
+std::vector<std::string> valid_ids;
 std::string string_chunk = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224, 1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+bool is_valid(std::string id_chunk, int id_len, enum int_type){
+    
+}
 
 void print_nl(){
     std::cout << std::endl;
 }
 
+bool is_even(int value){
+    if(value%2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void is_valid(Boundaries boundaries) {
+    int loop_int = 0; 
+    for(int i=boundaries.lower;i<boundaries.upper;i++){
+        std::string string_int = std::to_string(i);
+        int id_length = string_int.length();
+        if(is_even(id_length)){
+            int loop_upper = id_length/2;
+            for(int u=0;u<loop_upper;u++){
+                bool validity = is_valid(string_int,id_length, int_type::EVEN);
+            }
+        }
+    }
+}
 
 Boundaries get_boundary_conditions(std::string id_range){
     Boundaries  conditions;
@@ -65,13 +96,13 @@ int main() {
     std::string  range_separator = "-";
 
     for(int indx =0; indx<lenght_of_chunk; indx++){
-        id += string_chunk[indx];
         current_char = string_chunk[indx];
         
         if(current_char==sep){
             ID_array.push_back(id);
-            current_char = nullptr;
             id="";
+        }else{
+            id += string_chunk[indx];
         }
     }
     std::cout << "ID array: ";
