50475bc6690a6c9cf1b7f2c3368b64324f5de717
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-01-24 14:49:13 +0000

    [TESTED+WORKING] Wrote line to associate the @ to X

--- c++/2025/day4.cpp
+++ c++/2025/day4.cpp
@@ -31,22 +31,35 @@ std::vector<std::string> toilet_paper_array = {
 };
 
 
+void compose_map(Counter counter){
+    int number_of_rolls = 10;
+}
+
 void recognise_symbols() {
     Counter counter;
+    std::string map;
     std::cout << "Counter value: " <<  counter.val <<std::endl;
 
     for (int i=0; i<toilet_paper_array.size();i++){
         std::string line = toilet_paper_array[i];
+        int line_len = line.size() ;
         std::cout << toilet_paper_array[i]<< std::endl;
-        for(int j=0;j<line.size();j++){
+        
+        for(int j=0;j<line_len;j++){
             if(line[j]=='@'){
                 std::cout << "@ found"<< std::endl;
                 counter.add(1);
+                map+="X";
             }else if(line[j]=='.'){
+                
                 counter.print();
-
+                map+=".";
                 std::cout << "Found ." << std::endl;
                 counter.reset();
+            }
+            if(j+1==line_len){
+                std::cout << map<<std::endl;
+                map="";
             }
         }
     }
