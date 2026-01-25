a687ca155f67687ca4f7a227b230c081c48f82d3
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-01-25 12:16:31 +0000

    [BUG] Wrote check system for above, below, right and left of the array

--- c++/2025/day4.cpp
+++ c++/2025/day4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
+#include <format>
 
 struct Counter {
     int val = 0;
@@ -52,6 +52,10 @@ std::vector<std::string> convert_to_vector(int line_len, std::string line){
 void recognise_symbols() {
     Counter counter;
     std::string map;
+    char above;
+    char below;
+    char left;
+    char right;
     //std::cout << "Counter value: " <<  counter.val <<std::endl;
 
    // Convert to vector
@@ -59,9 +63,37 @@ void recognise_symbols() {
         std::string line = toilet_paper_array[i];
         int line_len = toilet_paper_array[i].size();
         std::vector<std::string> char_array =  convert_to_vector(line_len, line);
+        
         for(int j=0;j<char_array.size();j++){
-            std::cout << char_array[j] << std::endl;
-
+            std::cout << "j is currently: " << j<<std::endl;
+            std::cout << char_array[j] << "\n";
+            // Check up above i=0
+            if(i!=0){
+                above = toilet_paper_array[i-1][j];
+                std::string s = std::format("Checking above: toilet_paper_array[{0}][{1}]\n",i-1,j );
+                std::cout << s;
+                if(toilet_paper_array[i][j]=='@'){
+                    // write to map
+                }
+            }            
+            // check below
+            if(i!=line_len-1){
+                below = toilet_paper_array[i+1][j];
+                std::string s = std::format("Checking below: toilet_paper_array[{0}][{1}]\n",i+1 , j);
+                std::cout << s;
+            }
+            // check right
+            if(j!=char_array.size()){
+                right = toilet_paper_array[i][j+1];
+                std::string s = std::format("Checking below: toilet_paper_array[{0}][{1}]\n",i , j+1);
+                std::cout << s;
+            }
+            // check left
+            if(j!=0){
+                left = toilet_paper_array[i][j-1];
+                std::string s = std::format("Checking below: toilet_paper_array[{0}][{1}]\n",i , j-1);
+                std::cout << s;
+            }
         }
     }
 
