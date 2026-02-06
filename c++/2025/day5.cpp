6eacd114780c8e98b1952208103102573d2873a0
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-02-06 19:04:06 +0000

    [Added] Boundary struct for lower and upper bound

--- c++/2025/day5.cpp
+++ c++/2025/day5.cpp
@@ -1,6 +1,11 @@
 #include <vector>
 #include "useful_funcs.h"
 
+struct Boundary {
+    int upper;
+    int lower;
+};
+
 std::vector<std::string> input_arr = {
     "3-5",
     "10-14",
@@ -16,10 +21,18 @@ std::vector<std::string> input_arr = {
 };
 
 
+void id_boundaries() {
+
+
+
+}
+
+
 int main() {
     std::cout << "Hello day 5";
     print_nl();
-    
+
+    id_boundaries();
 
     return 0;
 }
