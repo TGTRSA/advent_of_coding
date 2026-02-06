aa111ed3167aa4c553c13748f039a2edac5d9d7d
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-02-06 19:02:27 +0000

    [START] DAY5

---
+++ c++/2025/day5.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include "useful_funcs.h"
+
+std::vector<std::string> input_arr = {
+    "3-5",
+    "10-14",
+    "16-20",
+    "12-18",
+    "",
+    "1",
+    "5",
+    "8",
+    "11",
+"17",
+"32",
+};
+
+
+int main() {
+    std::cout << "Hello day 5";
+    print_nl();
+    
+
+    return 0;
+}
