9d5af8126095e7b7b5b00efb22110e1df5ad5575
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-03-18 17:11:03 +0000

    [Added] Code to reorder the list in a matrix of sorts

--- c++/2025/day6.cpp
+++ c++/2025/day6.cpp
@@ -2,21 +2,42 @@
 #include <vector>
 #include <iostream>
 #include "useful_funcs.h"
+// cases: * + * +
+
 std::vector<std::string> h_list = {
-    "123", "328","  51"," 64",
+    "123", "328","51","64",
     "45","64",  "387"," 23",
-    "6"," 98",  "215", "314",
+    "6","98",  "215", "314",
     "*",  "+",   "*",   "+"
 };
 
 
 int main() {
     std::cout << "Starting program\n";
-
+    int counter = 0;
     size_t len_list = h_list.size();
+    std::vector<std::vector<int>> matrix;
     for(size_t i=0;i<len_list;i++){
-        std::cout << h_list[i] << ",";
+        std::string c = h_list[i];
+        std::cout << c << ",";
+        if(c=="+"){
+
+        }else if (c=="*") {
+        
+        }else {
+            int val = std::stoi(c);
+            matrix[counter].push_back(val);
+            counter++;
+            if (counter==3){counter=0;}
+        }
     }
-    print_nl();
+    // testing values to see if theyre correctly added to list 
+    size_t len_matrix = matrix.size();
+    for(size_t i=0;i<len_matrix;i++){
+        
+    }
+
+
+
 
 }
