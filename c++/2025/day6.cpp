c634f11f5745fc19ccfedfa9805cf92cf868bcce
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-03-18 17:45:15 +0000

    [ADDED, CHANGED] Vector for containing the operations. Decided to switch out the weird matrix for a map instead

--- c++/2025/day6.cpp
+++ c++/2025/day6.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <map>
 #include <vector>
 #include <iostream>
 #include "useful_funcs.h"
@@ -12,31 +13,66 @@ std::vector<std::string> h_list = {
 };
 
 
+std::map<int, std::vector<int>> equations;
+
 int main() {
     std::cout << "Starting program\n";
-    int counter = 0;
+
+    std::vector<std::string> operations;
+    int counter = 1;
     size_t len_list = h_list.size();
-    std::vector<std::vector<int>> matrix;
     for(size_t i=0;i<len_list;i++){
         std::string c = h_list[i];
         std::cout << c << ",";
-        if(c=="+"){
-
-        }else if (c=="*") {
-        
+        if(c=="+" or c=="*"){
+            operations.push_back(c);
         }else {
             int val = std::stoi(c);
-            matrix[counter].push_back(val);
+            switch (counter) {
+                case 1:
+                {
+                    equations[1].push_back(val);
+                    std::cout << "Appending at "<< counter << "\n";
+                    break;
+                }
+                case 2:
+                {
+                    equations[2].push_back(val);
+                    std::cout << "Appending at "<< counter << "\n";
+
+                    break;
+                
+                }
+                case 3:
+                {
+                    equations[3].push_back(val);
+                    std::cout << "Appending at "<< counter << "\n";
+                    break;
+                }
+                case 4:
+                {
+                    equations[4].push_back(val);
+                    std::cout << "Appending at "<< counter << "\n";
+                    counter=0;
+                    break;
+                }
+            }
             counter++;
-            if (counter==3){counter=0;}
         }
     }
-    // testing values to see if theyre correctly added to list 
-    size_t len_matrix = matrix.size();
-    for(size_t i=0;i<len_matrix;i++){
-        
+    print_nl();
+    size_t len_map = equations.size();
+    for(size_t i=0;i<len_map;i++){
+        size_t n=i+1;
+        size_t len_inner = equations.at(n).size();
+        std::cout << "Len inner map: " << len_inner;
+        for(size_t j=0;j<len_inner;j++){
+            std::cout << equations[i][j];
+            if (equations[i][j]==std::stoi(h_list[j])) {
+                std::cout << "Values match";
+            }
+        }
     }
-
 
 
 
