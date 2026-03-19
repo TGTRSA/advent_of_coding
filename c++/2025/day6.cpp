32646d7dbbb58c94f5380a91bb59142304c2fb65
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-03-19 19:51:52 +0000

    [Added] Code to conduct the operations required

--- c++/2025/day6.cpp
+++ c++/2025/day6.cpp
@@ -11,6 +11,7 @@ std::vector<std::string> h_list = {
     "6","98",  "215", "314",
     "*",  "+",   "*",   "+"
 };
+std::vector<int> answers={33210,490, 424355,401};
 
 
 std::map<int, std::vector<int>> equations;
@@ -21,6 +22,7 @@ int main() {
     std::vector<std::string> operations;
     int counter = 1;
     size_t len_list = h_list.size();
+    // appending values to map
     for(size_t i=0;i<len_list;i++){
         std::string c = h_list[i];
         std::cout << c << ",";
@@ -63,6 +65,7 @@ int main() {
     print_nl();
     size_t len_map = equations.size();
     int count = 0;
+    // matching values test
     for(size_t i=1;i<len_map+1;i++){
         // size_t n=i+1;
         std::vector<int>& inner_map = equations[i];
@@ -77,12 +80,40 @@ int main() {
                 std::cout << "Values match";
                 count+=4;
                 break;
+            }else {
+                std::cout << "VALUE DOES NOT MATCH\nEXITING..." << "\n";
+                exit(0);
             }
             
         }
         print_nl();
     }
-
+    // grouping into equation
+    int type=0;
+    std::string plus = "+";
+    std::string multiply = "*";
+    std::vector<int> results;
+    int result;
+    for(size_t i=1;i<len_map+2;i++){
+        std::string operation = operations[i-1];
+        
+        for(size_t j=0;j<len_map+1;j++){
+            int value = equations[i][j];
+            if(i==1){
+                result = value;
+            }else{
+                if (operation==plus) {
+                    result+=value;
+                }else if (operation==multiply) {
+                    result = result * value;
+                }
+            }
+        }
+        results.push_back(result);
+        result=0;
+    }
+    // comparing results with the answers
+    
 
 
 }
